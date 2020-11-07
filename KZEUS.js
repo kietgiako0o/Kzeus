2d2ea51c1ee54ef4b4569f6730aa300c76509c4b
kietgiako0o <pap10kiet09@gmail.com>
2020-11-07 14:01:15 +0700

    first commit

---
+++ KZEUS.js
@@ -0,0 +1,22 @@
+let btnSubmit = document.getElementById('buttonsubmit');
+
+btnSubmit.addEventListener('click', (e) =>{
+    e.preventDefault();
+    let kname = document.getElementById('kname').value;
+    let kpass = document.getElementById('kpass').value;
+    console.log(kname);
+    console.log(kpass);
+    if(!kname.Contains('@')){
+        console.log("thêm ký tự @ đi");
+        alert('tài khoản méo tồn tại')
+    }
+    else{
+    // document.getElementById("form").submit();
+    if(password === "123"){
+        DOM.
+    }
+}
+});
+
+// lùi lại để tìm file
+window.location.href = "./hihi/home.html";
